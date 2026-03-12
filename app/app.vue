876dbdc0b9443d8f7f7a4e6abb92aa75784ee24b
Bartolumiu <bartolumiu@tr25.es>
2026-03-12 23:17:33 +0100

    feat(app): add home link to header title

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen bg-gray-50 text-gray-900 font-sans">
     <header class="bg-white shadow-sm py-6 px-4 md:px-8 mb-8">
-      <h1 class="text-3xl font-bold text-primary-600">Recipes Explorer</h1>
+      <h1 class="text-3xl font-bold text-primary-600"><NuxtLink to="/">Recipes Explorer</NuxtLink></h1>
       <p class="text-gray-500 mt-2">I was feeling hungry :)</p>
     </header>
 
