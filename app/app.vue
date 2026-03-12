37186bbe4ff73e4e12389f7e85727e2d9d08a15f
Bartolumiu <bartolumiu@tr25.es>
2026-03-12 22:10:25 +0100

    feat(ui): add recipes view, Tailwind CSS and styles

---
+++ app/app.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <header>
+      <h1>Recipes Explorer</h1>
+      <p>I was feeling hungry :)</p>
+    </header>
+
+    <main>
+      <div>
+        <!-- Placeholder for cards -->
+      </div>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const recipes = ref([
+  {
+    id: 1,
+    title: 'Marmitako (Tuna Stew)',
+    time: '45 mins',
+    difficulty: 'Medium',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/1/1b/Marmitako.JPG',
+    image_attribution: 'Núria Pueyo, CC BY-SA 2.5 <https://creativecommons.org/licenses/by-sa/2.5>, via Wikimedia Commons'
+  },
+  {
+    id: 2,
+    title: 'Cocotxas de bacalao al pilpil (Cod cheeks in pilpil sauce)',
+    time: '45 mins',
+    difficulty: 'Hard',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/b/b8/CocotxasPilPil.jpg',
+    image_attribution: 'BocaDorada, CC BY-SA 2.0 <https://creativecommons.org/licenses/by-sa/2.0>, via Wikimedia Commons'
+  },
+  {
+    id: 3,
+    title: 'Basque Cheesecake',
+    time: '60 mins',
+    difficulty: 'Medium',
+    image: 'https://upload.wikimedia.org/wikipedia/commons/3/34/Basque-cheesecake.jpg',
+    image_attribution: 'The-Wuje, CC BY-SA 4.0 <https://creativecommons.org/licenses/by-sa/4.0>, via Wikimedia Commons'
+  }
+]);
+</script>
